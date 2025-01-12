1f772dbffb9c15f5a2e798e7d7f62104d57c717e
Ryeuuk <adelpq1@gmail.com>
2025-01-12 18:17:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-
 document.getElementById('extractButton').addEventListener('click', function () {
     const text = document.getElementById('textInput').value;
     const ipRegex = /\b(?:\d{1,3}\.){3}\d{1,3}\b/g;
     const ips = text.match(ipRegex);
 
     const outputElement = document.getElementById('output');
-    outputElement.innerText = ips ? ips.join('\n') : 'No IPs found.';
+    const copyButton = document.getElementById('copyButton');
+
+    if (ips) {
+        outputElement.innerText = ips.join('\n');
+        copyButton.classList.remove('hidden');
+    } else {
+        outputElement.innerText = 'No IPs found.';
+        copyButton.classList.add('hidden');
+    }
+});
+
+document.getElementById('copyButton').addEventListener('click', function () {
+    const outputText = document.getElementById('output').innerText;
+    navigator.clipboard.writeText(outputText)
+        .then(() => {
+            alert('IPs copied to clipboard!');
+        })
+        .catch(err => {
+            alert('Failed to copy text: ', err);
+        });
 });
